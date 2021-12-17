8a5b1bc9f293503e71d5aa3e1915310b5b77ad98
Shivammishra789 <shivammishra7789@gmail.com>
2021-12-17 20:01:01 +0530

    ADDED: calculated employee wage for multiple companies

--- employee_wage_problem.py
+++ employee_wage_problem.py
@@ -1,24 +1,23 @@
-'''Refactored the Code to write a Class Method to Compute Employee wage
-   (Class method and class variables used)'''
+'''Computing employee wage for multiple companies which are having their own wage,
+ number of working days and working hours per month'''
 import random
 
 
 class EmployeeWage:
-    WAGE_PER_HOUR = 20
-    MONTHLY_WORKING_DAYS = 20
-    MONTHLY_WORKING_HRS = 100
+
     FULL_TIME = 8
     PART_TIME = 4
     ABSENT = 0
 
-
-    def calculate_employee_wage(self):
-        """" Calculating employee wage """
+    # passing parameters to function
+    def calculate_employee_wage(self, company, wage_per_hour, monthly_working_days, max_hours_per_month):
+        """" Calculating employee wage with parameters passed"""
         monthly_emp_wage = 0
         emp_working_days = 0
         emp_working_hrs = 0
+
         # loop runs till monthly working days 20 or monthly working hrs 100 is reached
-        while emp_working_days < self.MONTHLY_WORKING_DAYS and emp_working_hrs <= self.MONTHLY_WORKING_HRS:
+        while emp_working_days < monthly_working_days and emp_working_hrs <= max_hours_per_month:
             emp_working_days += 1
 
             # generating random number
@@ -36,10 +35,14 @@ class EmployeeWage:
                 emp_working_hrs -= emp_hrs
                 break
             print('Day:', emp_working_days, 'Hrs Present:', emp_hrs)
-            monthly_emp_wage = self.WAGE_PER_HOUR * emp_working_hrs
+            monthly_emp_wage = wage_per_hour * emp_working_hrs
 
         print('Total working hours:', emp_working_hrs)
-        print('Monthly employee wage is:', monthly_emp_wage)
+        print('Monthly employee wage for company',company,"is", monthly_emp_wage)
 
-employee = EmployeeWage()
-employee.calculate_employee_wage()
+
+# creating objects and passing values to method
+employee1 = EmployeeWage()
+employee1.calculate_employee_wage('Tata',20,20,100)
+employee2 = EmployeeWage()
+employee2.calculate_employee_wage('Sinclair',25,30,100)
